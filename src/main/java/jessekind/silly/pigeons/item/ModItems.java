d1ab45f570891d94a010492ab6d22e0c1c339bba
Jesse Kind <d279759@edu.rocwb.nl>
2023-05-04 01:34:43 +0200

    Added pigeon eggs drop upon breeding

--- src/main/java/jessekind/silly/pigeons/item/ModItems.java
+++ src/main/java/jessekind/silly/pigeons/item/ModItems.java
@@ -16,9 +16,12 @@ public class ModItems {
 
     public static final Item PIGEON_FEATHER = registerItem("pigeon_feather",
         new Item(new FabricItemSettings()));
+    public static final Item PIGEON_EGG = registerItem("pigeon_egg",
+        new Item(new FabricItemSettings()));
     public static final Item CITY_PIGEON_SPAWN_EGG = registerItem("city_pigeon_spawn_egg",
         new SpawnEggItem(ModEntities.CITY_PIGEON, 0xffffff, 0x6a608c, new FabricItemSettings()));
-    
+
+        
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(SillyPigeons.MOD_ID, name), item);
     }
@@ -26,6 +29,10 @@ public class ModItems {
     public static void addItemsToItemGroup() {
         addToItemGroup(ItemGroups.INGREDIENTS, PIGEON_FEATHER);
         addToItemGroup(ModItemGroup.SILLY_PIGEONS, PIGEON_FEATHER);
+
+        addToItemGroup(ItemGroups.INGREDIENTS, PIGEON_EGG);
+        addToItemGroup(ModItemGroup.SILLY_PIGEONS, PIGEON_EGG);
+
         addToItemGroup(ItemGroups.SPAWN_EGGS, CITY_PIGEON_SPAWN_EGG);
         addToItemGroup(ModItemGroup.SILLY_PIGEONS, CITY_PIGEON_SPAWN_EGG);
     }
@@ -36,7 +43,6 @@ public class ModItems {
 
     public static void registerModItems() {
         SillyPigeons.LOGGER.info("Registering Mod Items for" + SillyPigeons.MOD_ID);
-
         addItemsToItemGroup();
     }
 
